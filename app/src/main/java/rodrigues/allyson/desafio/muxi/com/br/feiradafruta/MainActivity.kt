fa6e4280e0db8626e0ffcaaf48d51bd0edea4720
Allyson <allyson.b.rodrigues@gmail.com>
2018-07-26 22:43:54 -0300

    First commit

---
+++ app/src/main/java/rodrigues/allyson/desafio/muxi/com/br/feiradafruta/MainActivity.kt
@@ -0,0 +1,12 @@
+package rodrigues.allyson.desafio.muxi.com.br.feiradafruta
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
